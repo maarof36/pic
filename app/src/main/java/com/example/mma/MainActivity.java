a1f01eccf4876e2dd332bfaabfaae73eecbb8115
andrio <abdelhadeadan@gmail.com>
2023-11-21 12:01:21 +0200

    profile pic shape update

--- app/src/main/java/com/example/mma/MainActivity.java
+++ app/src/main/java/com/example/mma/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.mma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    FloatingActionButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getColor(R.color.blue)));
+
+        button = findViewById(R.id.floatingActionButton);
+        imageView = findViewById(R.id.imageView3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
